7e71543bf480599c05b0d93d00bd40186507f422
nano <152216368+AverageMaipuperson@users.noreply.github.com>
2025-09-12 20:18:34 -0300

    Add files via upload

---
+++ seafuncs/settings/config.php
@@ -0,0 +1,7 @@
+<?php
+$needsAdmin = false; // Set true if only admins can execute code, set false if not.
+
+$colorCoding = 2; // 1 for classic, 2 for alternate, any other number if disabled
+
+$reportErrors = true; // if set to true, it will stop code execution and show an exception
+?>
